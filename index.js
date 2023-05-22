231bea15a168502f45c8b7c9078f9956bc48f129
thejjwang <jonathanjacobwang@gmail.com>
2023-05-22 12:33:08 -0700

    add function works properly

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ newArr = []
 input.addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
     myFunction();
+    renderTasks();
+    input.value = "";
   }
 });
 function myFunction(){
-    console.log("hello world");
+    newArr.push(input.value);
+    console.log(newArr)
 }
+function renderTasks(){
+    ul.innerHTML = "";
+    for (let i = 0; i < newArr.length; i++){
+        ul.innerHTML += `<li>${newArr[i]}</li>`
+        
+    }
+}
+
